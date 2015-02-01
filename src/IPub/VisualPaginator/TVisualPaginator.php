9fae10a211b9e8055d2f5c27dd08e065e695222d
Adam Kadlec <adam.kadlec@fastybird.com>
2015-02-01 21:41:12 +0100

    - Created trait for easier usage of extension

---
+++ src/IPub/VisualPaginator/TVisualPaginator.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * TVisualPaginator.php
+ *
+ * @copyright	More in license.md
+ * @license		http://www.ipublikuj.eu
+ * @author		Adam Kadlec http://www.ipublikuj.eu
+ * @package		iPublikuj:VisualPaginator!
+ * @subpackage	common
+ * @since		5.0
+ *
+ * @date		01.02.15
+ */
+
+namespace IPub\VisualPaginator;
+
+use Nette;
+use Nette\Application;
+
+use IPub;
+use IPub\VisualPaginator\Components;
+
+trait TVisualPaginator
+{
+	/**
+	 * @var Components\IControl
+	 */
+	protected $visualPaginatorFactory;
+
+	/**
+	 * @param Components\IControl $visualPaginatorFactory
+	 */
+	public function injectAssetsLoader(Components\IControl $visualPaginatorFactory) {
+		$this->visualPaginatorFactory = $visualPaginatorFactory;
+	}
+}
