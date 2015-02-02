22e493159b50b45fd025f11715fd35c1a348225f
Adam Kadlec <adam.kadlec@fastybird.com>
2015-02-03 00:52:59 +0100

    - Fixed phpdocumentator parts

--- src/IPub/VisualPaginator/Components/Control.php
+++ src/IPub/VisualPaginator/Components/Control.php
@@ -23,6 +23,16 @@ use IPub;
 use IPub\VisualPaginator;
 use IPub\VisualPaginator\Exceptions;
 
+/**
+ * Visual paginator control
+ *
+ * @package		iPublikuj:VisualPaginator!
+ * @subpackage	Components
+ *
+ * @method onShowPage(Control $self, $page)
+ *
+ * @property-read Application\UI\ITemplate $template
+ */
 class Control extends Application\UI\Control
 {
 	/**
@@ -70,26 +80,32 @@ class Control extends Application\UI\Control
 	 */
 	public function render()
 	{
-		// Assign vars to template
-		$this->template->steps		= $this->getSteps();
-		$this->template->paginator	= $this->getPaginator();
-		$this->template->handle		= 'showPage!';
-		$this->template->useAjax	= $this->useAjax;
+		// Check if control has template
+		if ($this->template instanceof Nette\Bridges\ApplicationLatte\Template) {
+			// Assign vars to template
+			$this->template->steps		= $this->getSteps();
+			$this->template->paginator	= $this->getPaginator();
+			$this->template->handle		= 'showPage!';
+			$this->template->useAjax	= $this->useAjax;
 
-		// Check if translator is available
-		if ($this->getTranslator() instanceof Localization\ITranslator) {
-			$this->template->setTranslator($this->getTranslator());
+			// Check if translator is available
+			if ($this->getTranslator() instanceof Localization\ITranslator) {
+				$this->template->setTranslator($this->getTranslator());
+			}
+
+			// If template was not defined before...
+			if ($this->template->getFile() === NULL) {
+				// ...try to get base component template file
+				$templatePath = !empty($this->templatePath) ? $this->templatePath : __DIR__ . DIRECTORY_SEPARATOR .'template'. DIRECTORY_SEPARATOR .'default.latte';
+				$this->template->setFile($templatePath);
+			}
+
+			// Render component template
+			$this->template->render();
+
+		} else {
+			throw new Exceptions\InvalidStateException('Visual paginator control is without template.');
 		}
-
-		// If template was not defined before...
-		if ($this->template->getFile() === NULL) {
-			// ...try to get base component template file
-			$templatePath = !empty($this->templatePath) ? $this->templatePath : __DIR__ . DIRECTORY_SEPARATOR .'template'. DIRECTORY_SEPARATOR .'default.latte';
-			$this->template->setFile($templatePath);
-		}
-
-		// Render component template
-		$this->template->render();
 	}
 
 	/**
